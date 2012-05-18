f416d87db51fcf0f5045919491841b2a8eac4fa8
Uzi Landsmann <uzi.landsmann@gmail.com>
2012-05-18 21:42:46 +0200

    added test spec

---
+++ src/test/scala/examples/TestSpec.scala
@@ -0,0 +1,14 @@
+package examples
+
+import org.specs2.mutable.Specification
+import dispatch._
+
+class TestSpec extends Specification {
+
+  "Test Service" should {
+    "should work" in {
+      success
+    }
+  }
+
+}
